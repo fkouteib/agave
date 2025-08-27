fbb5f57c1174e68c57bdceb56821db7abefaf365
Faycel Kouteib <faycel.kouteib@anza.xyz>
2025-08-27 03:14:41 +0000

    bump up threads to 16. still oom at 12

--- rpc/src/transaction_status_service.rs
+++ rpc/src/transaction_status_service.rs
@@ -57,7 +57,7 @@ const TSS_TEST_QUIESCE_NUM_RETRIES: usize = 100;
 #[cfg(feature = "dev-context-only-utils")]
 const TSS_TEST_QUIESCE_SLEEP_TIME_MS: u64 = 50;
 
-const NUM_TSS_WORKER_THREADS: usize = 12;
+const NUM_TSS_WORKER_THREADS: usize = 16;
 
 const TSS_MESSAGES_DEFAULT_BATCH_SIZE: usize = 2048;
 const TSS_MESSAGES_MAX_BATCH_SIZE: usize = TSS_MESSAGES_DEFAULT_BATCH_SIZE * 2;
