03b710cde28db3a90ea464c6a551f1d27d7693f5
Faycel Kouteib <faycel.kouteib@anza.xyz>
2025-09-09 16:57:14 +0000

    more cpu threads with multi-msg batching

--- rpc/src/transaction_status_service.rs
+++ rpc/src/transaction_status_service.rs
@@ -57,7 +57,7 @@ const TSS_TEST_QUIESCE_NUM_RETRIES: usize = 100;
 #[cfg(feature = "dev-context-only-utils")]
 const TSS_TEST_QUIESCE_SLEEP_TIME_MS: u64 = 50;
 
-const NUM_TSS_WORKER_THREADS: usize = 8;
+const NUM_TSS_WORKER_THREADS: usize = 16;
 
 const MESSAGE_BATCH_SIZE: usize = 40;
 
